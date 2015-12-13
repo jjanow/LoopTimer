40f203926e5bcab994e03123122b9b563debd973
Jimmy <h4x3r@hotmail.com>
2015-12-12 22:23:33 -0500

    Various bug fixes and added a reset button

--- LoopTimer/Form1.Designer.cs
+++ LoopTimer/Form1.Designer.cs
@@ -28,193 +28,183 @@
         /// </summary>
         private void InitializeComponent()
         {
-			this.components = new System.ComponentModel.Container();
-			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
-			this.tbMinutes = new System.Windows.Forms.TextBox();
-			this.btnGo = new System.Windows.Forms.Button();
-			this.cbLoop = new System.Windows.Forms.CheckBox();
-			this.lblMinutes = new System.Windows.Forms.Label();
-			this.timer1 = new System.Windows.Forms.Timer(this.components);
-			this.lblHours = new System.Windows.Forms.Label();
-			this.tbHours = new System.Windows.Forms.TextBox();
-			this.tbSeconds = new System.Windows.Forms.TextBox();
-			this.btnPause = new System.Windows.Forms.Button();
-			this.lblSeconds = new System.Windows.Forms.Label();
-			this.lblHr = new System.Windows.Forms.Label();
-			this.label2 = new System.Windows.Forms.Label();
-			this.lblMin = new System.Windows.Forms.Label();
-			this.label4 = new System.Windows.Forms.Label();
-			this.lblSec = new System.Windows.Forms.Label();
-			this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
-			this.SuspendLayout();
-			// 
-			// tbMinutes
-			// 
-			this.tbMinutes.Location = new System.Drawing.Point(83, 41);
-			this.tbMinutes.Name = "tbMinutes";
-			this.tbMinutes.Size = new System.Drawing.Size(44, 20);
-			this.tbMinutes.TabIndex = 2;
-			this.tbMinutes.Text = "0";
-			// 
-			// btnGo
-			// 
-			this.btnGo.Location = new System.Drawing.Point(228, 9);
-			this.btnGo.Name = "btnGo";
-			this.btnGo.Size = new System.Drawing.Size(54, 23);
-			this.btnGo.TabIndex = 4;
-			this.btnGo.Text = "Go";
-			this.btnGo.UseVisualStyleBackColor = true;
-			this.btnGo.Click += new System.EventHandler(this.btnGo_Click);
-			// 
-			// cbLoop
-			// 
-			this.cbLoop.AutoSize = true;
-			this.cbLoop.Location = new System.Drawing.Point(228, 67);
-			this.cbLoop.Name = "cbLoop";
-			this.cbLoop.Size = new System.Drawing.Size(56, 17);
-			this.cbLoop.TabIndex = 6;
-			this.cbLoop.Text = "Loop?";
-			this.cbLoop.UseVisualStyleBackColor = true;
-			// 
-			// lblMinutes
-			// 
-			this.lblMinutes.AutoSize = true;
-			this.lblMinutes.Location = new System.Drawing.Point(76, 19);
-			this.lblMinutes.Name = "lblMinutes";
-			this.lblMinutes.Size = new System.Drawing.Size(44, 13);
-			this.lblMinutes.TabIndex = 5;
-			this.lblMinutes.Text = "Minutes";
-			// 
-			// timer1
-			// 
-			this.timer1.Interval = 1000;
-			this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
-			// 
-			// lblHours
-			// 
-			this.lblHours.AutoSize = true;
-			this.lblHours.Location = new System.Drawing.Point(12, 19);
-			this.lblHours.Name = "lblHours";
-			this.lblHours.Size = new System.Drawing.Size(35, 13);
-			this.lblHours.TabIndex = 6;
-			this.lblHours.Text = "Hours";
-			// 
-			// tbHours
-			// 
-			this.tbHours.Location = new System.Drawing.Point(12, 41);
-			this.tbHours.Name = "tbHours";
-			this.tbHours.Size = new System.Drawing.Size(44, 20);
-			this.tbHours.TabIndex = 1;
-			this.tbHours.Text = "0";
-			// 
-			// tbSeconds
-			// 
-			this.tbSeconds.Location = new System.Drawing.Point(154, 41);
-			this.tbSeconds.Name = "tbSeconds";
-			this.tbSeconds.Size = new System.Drawing.Size(44, 20);
-			this.tbSeconds.TabIndex = 3;
-			this.tbSeconds.Text = "0";
-			// 
-			// btnPause
-			// 
-			this.btnPause.Enabled = false;
-			this.btnPause.Location = new System.Drawing.Point(228, 38);
-			this.btnPause.Name = "btnPause";
-			this.btnPause.Size = new System.Drawing.Size(54, 23);
-			this.btnPause.TabIndex = 5;
-			this.btnPause.Text = "Pause";
-			this.btnPause.UseVisualStyleBackColor = true;
-			this.btnPause.Click += new System.EventHandler(this.btnPause_Click);
-			// 
-			// lblSeconds
-			// 
-			this.lblSeconds.AutoSize = true;
-			this.lblSeconds.Location = new System.Drawing.Point(149, 19);
-			this.lblSeconds.Name = "lblSeconds";
-			this.lblSeconds.Size = new System.Drawing.Size(49, 13);
-			this.lblSeconds.TabIndex = 11;
-			this.lblSeconds.Text = "Seconds";
-			// 
-			// lblHr
-			// 
-			this.lblHr.Font = new System.Drawing.Font("Microsoft Sans Serif", 19F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.lblHr.Location = new System.Drawing.Point(25, 110);
-			this.lblHr.Name = "lblHr";
-			this.lblHr.Size = new System.Drawing.Size(50, 30);
-			this.lblHr.TabIndex = 12;
-			this.lblHr.Text = "00";
-			this.lblHr.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-			// 
-			// label2
-			// 
-			this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 19F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.label2.Location = new System.Drawing.Point(92, 110);
-			this.label2.Name = "label2";
-			this.label2.Size = new System.Drawing.Size(15, 30);
-			this.label2.TabIndex = 13;
-			this.label2.Text = ":";
-			// 
-			// lblMin
-			// 
-			this.lblMin.Font = new System.Drawing.Font("Microsoft Sans Serif", 19F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.lblMin.Location = new System.Drawing.Point(124, 110);
-			this.lblMin.Name = "lblMin";
-			this.lblMin.Size = new System.Drawing.Size(50, 30);
-			this.lblMin.TabIndex = 14;
-			this.lblMin.Text = "00";
-			this.lblMin.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-			// 
-			// label4
-			// 
-			this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 19F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.label4.Location = new System.Drawing.Point(191, 110);
-			this.label4.Name = "label4";
-			this.label4.Size = new System.Drawing.Size(15, 30);
-			this.label4.TabIndex = 15;
-			this.label4.Text = ":";
-			// 
-			// lblSec
-			// 
-			this.lblSec.Font = new System.Drawing.Font("Microsoft Sans Serif", 19F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.lblSec.Location = new System.Drawing.Point(223, 110);
-			this.lblSec.Name = "lblSec";
-			this.lblSec.Size = new System.Drawing.Size(50, 30);
-			this.lblSec.TabIndex = 16;
-			this.lblSec.Text = "00";
-			this.lblSec.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-			// 
-			// notifyIcon1
-			// 
-			this.notifyIcon1.BalloonTipText = "Timer complete!";
-			this.notifyIcon1.BalloonTipTitle = "Timer";
-			this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
-			this.notifyIcon1.Text = "Loop Timer";
-			this.notifyIcon1.Visible = true;
-			// 
-			// Form1
-			// 
-			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(292, 173);
-			this.Controls.Add(this.lblSec);
-			this.Controls.Add(this.label4);
-			this.Controls.Add(this.lblMin);
-			this.Controls.Add(this.label2);
-			this.Controls.Add(this.lblHr);
-			this.Controls.Add(this.lblSeconds);
-			this.Controls.Add(this.btnPause);
-			this.Controls.Add(this.tbSeconds);
-			this.Controls.Add(this.tbHours);
-			this.Controls.Add(this.lblHours);
-			this.Controls.Add(this.lblMinutes);
-			this.Controls.Add(this.cbLoop);
-			this.Controls.Add(this.btnGo);
-			this.Controls.Add(this.tbMinutes);
-			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-			this.Name = "Form1";
-			this.Text = "Loop Timer";
-			this.ResumeLayout(false);
-			this.PerformLayout();
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.tbMinutes = new System.Windows.Forms.TextBox();
+            this.btnGo = new System.Windows.Forms.Button();
+            this.cbLoop = new System.Windows.Forms.CheckBox();
+            this.lblMinutes = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.lblHours = new System.Windows.Forms.Label();
+            this.tbHours = new System.Windows.Forms.TextBox();
+            this.tbSeconds = new System.Windows.Forms.TextBox();
+            this.btnPause = new System.Windows.Forms.Button();
+            this.lblSeconds = new System.Windows.Forms.Label();
+            this.lblHr = new System.Windows.Forms.Label();
+            this.lblMin = new System.Windows.Forms.Label();
+            this.lblSec = new System.Windows.Forms.Label();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.btnReset = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tbMinutes
+            // 
+            this.tbMinutes.Location = new System.Drawing.Point(12, 95);
+            this.tbMinutes.Name = "tbMinutes";
+            this.tbMinutes.Size = new System.Drawing.Size(44, 20);
+            this.tbMinutes.TabIndex = 2;
+            this.tbMinutes.Text = "0";
+            // 
+            // btnGo
+            // 
+            this.btnGo.Location = new System.Drawing.Point(259, 10);
+            this.btnGo.Name = "btnGo";
+            this.btnGo.Size = new System.Drawing.Size(54, 23);
+            this.btnGo.TabIndex = 4;
+            this.btnGo.Text = "Go";
+            this.btnGo.UseVisualStyleBackColor = true;
+            this.btnGo.Click += new System.EventHandler(this.btnGo_Click);
+            // 
+            // cbLoop
+            // 
+            this.cbLoop.AutoSize = true;
+            this.cbLoop.Location = new System.Drawing.Point(259, 114);
+            this.cbLoop.Name = "cbLoop";
+            this.cbLoop.Size = new System.Drawing.Size(56, 17);
+            this.cbLoop.TabIndex = 6;
+            this.cbLoop.Text = "Loop?";
+            this.cbLoop.UseVisualStyleBackColor = true;
+            // 
+            // lblMinutes
+            // 
+            this.lblMinutes.AutoSize = true;
+            this.lblMinutes.Location = new System.Drawing.Point(9, 79);
+            this.lblMinutes.Name = "lblMinutes";
+            this.lblMinutes.Size = new System.Drawing.Size(44, 13);
+            this.lblMinutes.TabIndex = 5;
+            this.lblMinutes.Text = "Minutes";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // lblHours
+            // 
+            this.lblHours.AutoSize = true;
+            this.lblHours.Location = new System.Drawing.Point(9, 9);
+            this.lblHours.Name = "lblHours";
+            this.lblHours.Size = new System.Drawing.Size(35, 13);
+            this.lblHours.TabIndex = 6;
+            this.lblHours.Text = "Hours";
+            // 
+            // tbHours
+            // 
+            this.tbHours.Location = new System.Drawing.Point(12, 25);
+            this.tbHours.Name = "tbHours";
+            this.tbHours.Size = new System.Drawing.Size(44, 20);
+            this.tbHours.TabIndex = 1;
+            this.tbHours.Text = "0";
+            // 
+            // tbSeconds
+            // 
+            this.tbSeconds.Location = new System.Drawing.Point(12, 160);
+            this.tbSeconds.Name = "tbSeconds";
+            this.tbSeconds.Size = new System.Drawing.Size(44, 20);
+            this.tbSeconds.TabIndex = 3;
+            this.tbSeconds.Text = "0";
+            // 
+            // btnPause
+            // 
+            this.btnPause.Enabled = false;
+            this.btnPause.Location = new System.Drawing.Point(259, 40);
+            this.btnPause.Name = "btnPause";
+            this.btnPause.Size = new System.Drawing.Size(54, 23);
+            this.btnPause.TabIndex = 5;
+            this.btnPause.Text = "Pause";
+            this.btnPause.UseVisualStyleBackColor = true;
+            this.btnPause.Click += new System.EventHandler(this.btnPause_Click);
+            // 
+            // lblSeconds
+            // 
+            this.lblSeconds.AutoSize = true;
+            this.lblSeconds.Location = new System.Drawing.Point(7, 144);
+            this.lblSeconds.Name = "lblSeconds";
+            this.lblSeconds.Size = new System.Drawing.Size(49, 13);
+            this.lblSeconds.TabIndex = 11;
+            this.lblSeconds.Text = "Seconds";
+            // 
+            // lblHr
+            // 
+            this.lblHr.Font = new System.Drawing.Font("Microsoft Sans Serif", 19F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHr.Location = new System.Drawing.Point(62, 20);
+            this.lblHr.Name = "lblHr";
+            this.lblHr.Size = new System.Drawing.Size(190, 30);
+            this.lblHr.TabIndex = 12;
+            this.lblHr.Text = "00";
+            this.lblHr.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblMin
+            // 
+            this.lblMin.Font = new System.Drawing.Font("Microsoft Sans Serif", 19F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMin.Location = new System.Drawing.Point(62, 90);
+            this.lblMin.Name = "lblMin";
+            this.lblMin.Size = new System.Drawing.Size(190, 30);
+            this.lblMin.TabIndex = 14;
+            this.lblMin.Text = "00";
+            this.lblMin.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblSec
+            // 
+            this.lblSec.Font = new System.Drawing.Font("Microsoft Sans Serif", 19F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSec.Location = new System.Drawing.Point(62, 155);
+            this.lblSec.Name = "lblSec";
+            this.lblSec.Size = new System.Drawing.Size(190, 30);
+            this.lblSec.TabIndex = 16;
+            this.lblSec.Text = "00";
+            this.lblSec.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.BalloonTipText = "Timer complete!";
+            this.notifyIcon1.BalloonTipTitle = "Timer";
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "Loop Timer";
+            this.notifyIcon1.Visible = true;
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(259, 70);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(54, 23);
+            this.btnReset.TabIndex = 17;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(324, 201);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.lblSec);
+            this.Controls.Add(this.lblMin);
+            this.Controls.Add(this.lblHr);
+            this.Controls.Add(this.lblSeconds);
+            this.Controls.Add(this.btnPause);
+            this.Controls.Add(this.tbSeconds);
+            this.Controls.Add(this.tbHours);
+            this.Controls.Add(this.lblHours);
+            this.Controls.Add(this.lblMinutes);
+            this.Controls.Add(this.cbLoop);
+            this.Controls.Add(this.btnGo);
+            this.Controls.Add(this.tbMinutes);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Loop Timer";
+            this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -231,11 +221,10 @@
         private System.Windows.Forms.Button btnPause;
         private System.Windows.Forms.Label lblSeconds;
         private System.Windows.Forms.Label lblHr;
-        private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label lblMin;
-        private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label lblSec;
         private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.Button btnReset;
     }
 }
 
--- LoopTimer/Form1.cs
+++ LoopTimer/Form1.cs
@@ -11,15 +11,16 @@ namespace LoopTimer
 {
 	public partial class Form1 : Form
 	{
-		public int seconds = 0; // Seconds.
-		public int minutes = 0; // Minutes.
-		public int hours = 0;   // Hours.
+		public Int64 seconds = 0; // Seconds.
+		public Int64 minutes = 0; // Minutes.
+		public Int64 hours = 0;   // Hours.
 		public bool paused = false; // State of the timer [PAUSED/WORKING].
 
 		public Form1()
 		{
+            //event handlers to deal with attempts to enter non-integer values into the text boxes
 			InitializeComponent();
-			this.tbMinutes.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.tbSeconds_KeyDown);
+			this.tbSeconds.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.tbSeconds_KeyDown);
 			this.tbMinutes.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.tbMinutes_KeyDown);
 			this.tbHours.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.tbHours_KeyDown);
 		}
@@ -40,18 +41,23 @@ namespace LoopTimer
 
 		private void btnGo_Click(object sender, EventArgs e)
 		{
+            //Change display text on the go and pause buttons when go is pressed
+            //also initialize the time variables.  enable the pause button.
+            //start the timer.
 			if (btnGo.Text == "Go")
 			{
 				timer1.Enabled = true;
 				paused = false;
 				btnGo.Text = "Stop";
-				seconds = System.Convert.ToInt32(tbSeconds.Text);
-				minutes = System.Convert.ToInt32(tbMinutes.Text);
-				hours = System.Convert.ToInt32(tbHours.Text);
+				seconds = Convert.ToInt64(tbSeconds.Text);
+				minutes = Convert.ToInt64(tbMinutes.Text);
+				hours = Convert.ToInt64(tbHours.Text);
 				btnPause.Enabled = true;
 			}
 			else
 			{
+                //if the button is currently set to "stop" then flip it to go and halt the timer
+                //also set all the time variables to 0 and disable the pause button
 				btnGo.Text = "Go";
 				timer1.Enabled = false;
 				seconds = 0;
@@ -61,6 +67,8 @@ namespace LoopTimer
 			}
 		}
 
+        //these will prevent uses from entering anything but positive integers into the 
+        //time text boxes
 		private void tbSeconds_KeyDown(object sender, KeyPressEventArgs e)
 		{
 			if (!char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar)) e.Handled = true;
@@ -76,6 +84,7 @@ namespace LoopTimer
 			if (!char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar)) e.Handled = true;
 		}
 
+        //handle clicking the pause button.  pauses the timer and flips text on the button itself.
 		private void btnPause_Click(object sender, EventArgs e)
 		{
 			if (paused == false)
@@ -94,23 +103,25 @@ namespace LoopTimer
 			}
 		}
 
+        //handle decrementing the timer for each of its ticks.
 		private void timer1_Tick(object sender, EventArgs e)
 		{
 			if ((minutes == 0) && (hours == 0) && (seconds == 0))
 			{
 				if (cbLoop.Checked == true)
 				{
-					seconds = System.Convert.ToInt32(tbSeconds.Text);
-					minutes = System.Convert.ToInt32(tbMinutes.Text);
-					hours = System.Convert.ToInt32(tbHours.Text);
-					notifyIcon1.ShowBalloonTip(60000);
+					seconds = Convert.ToInt64(tbSeconds.Text);
+					minutes = Convert.ToInt64(tbMinutes.Text);
+					hours = Convert.ToInt64(tbHours.Text);
+					notifyIcon1.ShowBalloonTip(60000); //balloon tip for when the timer finishes its current countdown
 					return;
 				}
+
 				timer1.Enabled = false;
 				btnGo.Enabled = true;
 				notifyIcon1.ShowBalloonTip(60000);
 			}
-			else
+			else //handle the math as time counts down, converting minutes, seconds, etc. and update the labels
 			{
 				if (seconds < 1)
 				{
@@ -119,17 +130,16 @@ namespace LoopTimer
 					{
 						minutes = 59;
 						if (hours != 0)
-							hours -= 1;
-
+							hours--;
 					}
 					else
 					{
-						minutes -= 1;
+						minutes--;
 					}
 				}
 				else
 				{
-					seconds -= 1;
+					seconds--;
 				}
 					
 				lblHr.Text = hours.ToString();
@@ -137,5 +147,25 @@ namespace LoopTimer
 				lblSec.Text = seconds.ToString();
 			}            
 		}
-	}
+
+        //reset all time values and labels to 0, stop the timer, and reset the buttons to their starting values
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            seconds = 0;
+            minutes = 0;
+            hours = 0;
+
+            tbSeconds.Text = "0";
+            tbMinutes.Text = "0";
+            tbHours.Text = "0";
+
+            lblSec.Text = "0";
+            lblMin.Text = "0";
+            lblHr.Text = "0";
+
+            btnGo.Text = "Go";
+            timer1.Enabled = false;
+            btnPause.Enabled = false;
+        }
+    }
 }
